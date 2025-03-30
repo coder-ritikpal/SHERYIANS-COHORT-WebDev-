b7d72bd209d241901bba2bafb0d804a918a6b94a
coder-ritikpal <palritik156@gmail.com>
2025-03-30 20:29:22 +0530

    Modified color-generator

--- JS DOM-Manipulation/Color-Generator (PROJECT-1)/script.js
+++ JS DOM-Manipulation/Color-Generator (PROJECT-1)/script.js
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
+
 let section = document.querySelector('.color-section')
 let btn = document.querySelector('button')
+let h2=document.querySelector('h2')
 
 
 btn.addEventListener("click", function () {
@@ -9,20 +10,6 @@ btn.addEventListener("click", function () {
     let b = Math.floor(Math.random() * 256)
 
     section.style.backgroundColor = `rgb(${r},${g},${b})`
-    
+    h2.innerHTML = `rgb(${r},${g},${b})`;
 
-=======
-let section = document.querySelector('.color-section')
-let btn = document.querySelector('button')
-
-
-btn.addEventListener("click", function () {
-    let r = Math.floor(Math.random() * 256)
-    let g = Math.floor(Math.random() * 256)
-    let b = Math.floor(Math.random() * 256)
-
-    section.style.backgroundColor = `rgb(${r},${g},${b})`
-    
-
->>>>>>> ef78fc33d8f365fc2238ef23de97817f79f1f7ca
 })
