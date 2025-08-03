a4ae7caabbafb50fdfd171ee0eb458f9ded7113c
coderritikpal <palritik156@gmail.com>
2025-08-03 13:14:15 +0530

    New backend url

--- FULL-STACK PROJECT/Moody-Player/frontend/src/components/FacialExpression.jsx
+++ FULL-STACK PROJECT/Moody-Player/frontend/src/components/FacialExpression.jsx
@@ -50,7 +50,7 @@ export default function FacialExpressionApp({ setSongs }) {
       setExpression(`${expName.toUpperCase()} ${emojiMap[expName] || ''} (${(score * 100).toFixed(1)}%)`);
 
       try {
-        const res = await axios.get(`https://moddy-player-backend.onrender.com/songs?mood=${expName.toLowerCase()}`);
+        const res = await axios.get(`https://moody-music-backend-ic2j.onrender.com/songs?mood=${expName.toLowerCase()}`);
         setSongs(res.data.songs);
       } catch (error) {
         console.error('Error fetching songs:', error);
