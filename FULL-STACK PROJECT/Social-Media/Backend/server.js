94fc17245ae67188a3d7335485a8bf3f6b297fa1
coderritikpal <palritik156@gmail.com>
2025-08-13 10:35:05 +0530

    Moved posts-caption generator to backend folder

--- FULL-STACK PROJECT/Social-Media/Backend/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const app = require('./src/app')
-const connectDB = require('./src/db/db')
-const PORT = process.env.PORT || 3000;
-
-
-connectDB()
-
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
