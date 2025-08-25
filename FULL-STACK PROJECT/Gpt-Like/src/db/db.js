31eeecaf349b8e81f363139c93453d341cb85406
coderritikpal <palritik156@gmail.com>
2025-08-25 11:38:55 +0530

    Seperated backend file

--- FULL-STACK PROJECT/Gpt-Like/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");   
-
-function connectDB() {
-    mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected successfully"))
-    .catch(err => console.error("MongoDB connection error:", err));
-}
-
-module.exports = connectDB;
