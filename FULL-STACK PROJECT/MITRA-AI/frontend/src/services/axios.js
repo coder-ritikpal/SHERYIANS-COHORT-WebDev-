ad2e0db57b74c0d8adc22430640ee9d091361af2
coderritikpal <palritik156@gmail.com>
2025-09-06 21:24:31 +0530

    Added env variable of backend

--- FULL-STACK PROJECT/MITRA-AI/frontend/src/services/axios.js
+++ FULL-STACK PROJECT/MITRA-AI/frontend/src/services/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
+
+// Env se base URL pick karo
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: `${API_BASE_URL}/api`,
   withCredentials: true,
 });
-
-
-
 
 export default axiosInstance;
--- FULL-STACK PROJECT/MITRA-AI/frontend/src/services/socket.js
+++ FULL-STACK PROJECT/MITRA-AI/frontend/src/services/socket.js
@@ -1,10 +1,12 @@
-// src/services/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+// Env se base URL pick karo
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+
+const socket = io(API_BASE_URL, {
   withCredentials: true,
   transports: ["websocket"],
-   autoConnect: false,
+  autoConnect: false,
 });
 
 export default socket;
