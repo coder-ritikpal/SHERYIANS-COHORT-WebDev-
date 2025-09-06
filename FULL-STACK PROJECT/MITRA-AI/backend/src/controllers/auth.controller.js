e46e1c75a2d027ad1e17f5959b8bc939cc643aa1
coderritikpal <palritik156@gmail.com>
2025-09-06 23:36:07 +0530

    setting http true while sending token

--- FULL-STACK PROJECT/MITRA-AI/backend/src/controllers/auth.controller.js
+++ FULL-STACK PROJECT/MITRA-AI/backend/src/controllers/auth.controller.js
@@ -31,7 +31,13 @@ async function registerUser(req, res) {
       { id: newUser._id, username: newUser.username, email: newUser.email },
       process.env.JWT_SECRET,
     );
-    res.cookie("token", token);
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+});
+
 
     // Respond with same shape as login
     res.status(201).json({
@@ -77,7 +83,13 @@ async function loginUser(req, res) {
             { id: user._id, username: user.username, email: user.email },
             process.env.JWT_SECRET,
         );
-         res.cookie("token", token);
+         res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+});
+
 
         res.status(200).json({
             message: 'Login successful',
